ab80b03c4ec8df95a86fd62d4f3cdcc9b441cdcb
Shinnosuke at MELCHIOR <shinnosuke@gmail.com>
2012-10-22 20:39:52 +0900

    add create issue

--- rapidmine.rb
+++ rapidmine.rb
@@ -3,6 +3,8 @@ require 'yaml'
 require 'json'
 require 'open-uri'
 require 'pp'
+require 'uri'
+require 'net/https'
 
 OPT = OptionParser.new
 Version = "0.2 alpha"
@@ -22,15 +24,46 @@ class String
 end
 
 class Rapidmine
+
+  def http(method, url, data = nil)
+    uri = URI.parse(url)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    # https.ca_file = '/usr/share/ssl/cert.pem'
+    https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    https.verify_depth = 5
+
+    if data
+      dd = { :issue => { } }
+      data.each do |k,v|
+        if k == :key
+          dd[k.to_s] = v
+        else
+          dd[:issue][k] = v
+        end
+      end
+    end
+
+    https.start do
+      if method == :post
+        response = https.post( uri.path + "?" + uri.query, dd.to_json  , 'Content-Type' => "application/json" )
+      else
+        response = https.send( method, uri.path + "?" + uri.query )
+      end
+      return response.body
+    end
+
+  end
+
   def make_path(h)
     h[:key] = CONFIG["api"]
     m = h.delete(:model).to_s + ".json"
     path = CONFIG["url"] + m + "?"
     h.each{ |k,v|
-      path << ( k.to_s + "=" + v.to_s + "&" )
+      path << ( URI.encode(k.to_s) + "=" + URI.encode(v.to_s) + "&" )
     }
     path << "limit=100"
-     return path
+    return path
   end
   
   def get_model(model,opt = nil)
@@ -40,26 +73,53 @@ class Rapidmine
         h[k] = v
       }
     end
-    j = JSON.parse(open(make_path(h)).read)
+    j = JSON.parse(http(:get, make_path(h)))
     return j
   end
 
-  def list_issues(opt = nil)
-    j = get_model(:issues, opt)
-    j['issues'].each do |e|
-      puts %( #{e['id']}\t#{e['subject']} - [#{e['status']['name']}][#{e['priority']['name']}] )
+  def post_model(model,opt = nil, data = nil)
+    h = { :model => model }
+    data.merge!(opt)
+    data[:key] = CONFIG["api"]
+    j = JSON.parse(http(:post, make_path(h), data))
+    return j
+  end
+
+  def create(model, opt)
+    data = opt.delete(:data)
+    j = post_model(model, opt, data)
+    self.send("print_"+model.to_s, j)
+  end
+
+  def list(model, opt)
+    j = get_model(model, opt)
+    self.send("print_"+model.to_s, j)
+  end
+
+  def print_issues(j)
+    if j['issues']
+      j['issues'].each do |e|
+        puts %( #{e['id']}\t#{e['subject']} -- [#{e['assigned_to']['name'] if e['assigned_to']}][#{e['status']['name']}][#{e['priority']['name']}] )
+      end
+    elsif j['issue']
+      e = j['issue']
+      puts %( #{e['id']}\t#{e['subject']} -- [#{e['assigned_to']['name'] if e['assigned_to']}][#{e['status']['name']}][#{e['priority']['name']}] )
     end
   end
 
-  def list_projects(opt = nil)
-    j = get_model(:projects, opt)
+  def create_issues(opt = nil)
+    data = opt.delete(:data)
+    j = post_model(:model, opt, data)
+    pp j
+  end
+
+  def print_projects(j)
     j['projects'].each do |e|
       puts %( #{e['id']}\t#{e['name']} - #{e['description'].gsub(/\s/,'').minify } )
     end
   end
 
-  def list_users(opt = nil)
-    j = get_model(:users, opt)
+  def print_users(j)
     j['users'].each do |e|
       puts %( #{e['id']}\t#{e['firstname']} #{e['lastname']} <#{e['mail']}> )
     end
@@ -82,7 +142,7 @@ class Rapidmine
     unless pj =~ /\d+/
       get_model(:projects)['projects'].each do |el|
         if ["identifier", "description", "name"].map{ |e| el[e] =~ /#{pj}/ }.inject(false){ |m,x| m || x } then
-          return el["id"] 
+          return el["id"]
         end
       end
       return "nil"
@@ -94,22 +154,29 @@ class Rapidmine
   def run(cmd)
     cmd[:assigned_to_id] = change_user_to_id(cmd[:assigned_to_id]) if cmd[:assigned_to_id]
     cmd[:project_id] = change_project_to_id(cmd[:project_id]) if cmd[:project_id]
-    m = cmd.delete(:method)
-    self.send(m, cmd)
+    method = cmd.delete(:method).to_s
+    model = cmd.delete(:model).to_s
+    self.send(method, model, cmd)
   end
 
 end
 
 def print_help
   puts "ruby rapidmine.rb [OPT]"
-  puts "-l, --list (projects|users|issues)"
-  puts "-u, --users USER_NAME | USER_ID"
-  puts "-p, --project PROJECT_NAME | PROJECT_ID"
+  puts "-o, --open ISSUE_NUM : Open the issue on your default web browser"
+  puts "-l, --list (projects|users|issues) : List out your(or someone's) issues(or projects|users)."
+  puts "-c, --create (issues) : Create your(or someone's) issues. -s SUBJECT and -p PROJECT_NAME are needed."
+  puts "[-u, --users USER_NAME | USER_ID]"
+  puts "[-p, --project PROJECT_NAME | PROJECT_ID]"
   puts " * PROJECT_NAME and USER_NAME is compared by regular expression."
 end
 
-mine = Rapidmine.new
-CMD = {}
+CMD = {
+  :method => :list,
+  :model => "issues",
+  #:assigned_to_id => CONFIG["user"],
+  :data => { }
+}
 
 OPT.on("-h","--help") do
   print_help
@@ -117,7 +184,17 @@ OPT.on("-h","--help") do
 end
 
 OPT.on("-l","--list VAL") do |e|
-  CMD[:method] = "list_"+e
+  CMD[:method] = :list
+  CMD[:model] = e
+end
+
+OPT.on("-c","--create [VAL]") do |e|
+  CMD[:method] = :create
+  if e
+    CMD[:model] = e
+  else
+    CMD[:model] = "issues"
+  end
 end
 
 OPT.on("-u","--user VAL") do |e|
@@ -128,10 +205,28 @@ OPT.on("-p","--project VAL") do |e|
   CMD[:project_id] = e
 end
 
+OPT.on("-s","--subject VAL") do |e|
+  CMD[:data][:subject] = e
+end
+
+OPT.on("-S","--status VAL") do |e|
+  CMD[:data][:status_id] = e
+end
+
+OPT.on("-P","--priority VAL") do |e|
+  CMD[:data][:priority_id] = e
+end
+
+OPT.on("-T","--tracker VAL") do |e|
+  CMD[:data][:tracker_id] = e
+end
+
+OPT.on("-o","--open VAL") do |e|
+  system("open 'https://dev.has-key.org/issues/#{e}'")
+  exit
+end
+
 OPT.parse(ARGV)
 
-if ARGV.size == 0 then
-  print_help
-else
-  mine.run(CMD)
-end
+mine = Rapidmine.new
+mine.run(CMD)
